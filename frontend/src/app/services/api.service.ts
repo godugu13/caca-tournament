56b721bc4dcd3d3241ffaee4cbe8465b1e742c43
caca <godugu@gmail.com>
2026-05-27 02:36:42 -0400

    Import Angular environment in api service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Match, Registration, Standing, Tournament, Member, PlayerScoreLookupResponse, DashboardTournament } from '../models/models';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
